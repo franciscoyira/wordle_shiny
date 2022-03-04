19b227e0b681d852e83676b0e5e94df27038e0c6
Francisco Yirá Albornoz <francisco.yira@outlook.com>
2022-03-04 11:55:46 -0300

    remove template app

--- app.R
+++ app.R
@@ -1,49 +1,81 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
+source("wordlist.R")
+
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+  textInput("guess", ""),
+  actionButton("go", "Go"),
+  verbatimTextOutput("result", placeholder = TRUE)
 )
 
-# Define server logic required to draw a histogram
+set.seed(as.integer(Sys.Date()))
+target <- sample(words_common, 1)
+
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+  
+  output$result <- renderPrint({
+    if (!(input$guess %in% words_all)) {
+      req(FALSE, cancelOutput = TRUE)
+    }
+    
+    result <- check_words(target, input$guess)
+    format_result(result)
+    
+  }) |> 
+    bindEvent(input$go)
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+format_result <- function(r) {
+  out_str <- ""
+  for (i in seq_along(r$letters)) {
+    if (r$result[i] == "correct") {
+      out_str <- paste0(out_str, "[", r$letters[i], "]")
+    } else if (r$result[i] == "in-word") {
+      out_str <- paste0(out_str, "(", r$letters[i], ")")
+    } else {
+      out_str <- paste0(out_str, " ", r$letters[i], " ")
+    }
+  }
+  out_str
+}
+
+compare_words <- function(target_str, guess_str) {
+  if (nchar(target_str) != nchar(guess_str)) {
+    stop("target and guess must be the same length.")
+  }
+  
+  target <- strsplit(target_str, "")[[1]]
+  guess <- strsplit(guess_str, "")[[1]]
+  result <- character(nchar(guess_str))
+  
+  for (i in seq_along(target)){
+    
+    if (guess[i] == target[i]) {
+      result[i] <- "correct"
+    } else if (guess[i] %in% target) {
+      result[i] <- "in-word"
+    } else {
+      result[i] <- "not-in-word"
+    }
+  }
+  
+  result
+}
+
+# A new function that returns a data structure with more information
+check_words <- function(target_str, guess_str) {
+  compare_result <- compare_words(target_str, guess_str)
+  correct <- FALSE
+  if (all(compare_result == "correct")) {
+    correct <- TRUE
+  }
+  
+  list(
+    guess = guess_str,
+    letters = strsplit(guess_str, "")[[1]],
+    result = compare_result,
+    correct = correct
+  )
+}
+
+shinyApp(ui, server)
